21b0626d4be2f2bdafbe3e412c9d96b3aeea81fb
John Chaffee <johnchaffee@mac.com>
2021-10-03 12:26:38 -0700

    Remove ejs

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const WebSocketServer = WebSocket.Server;
 const http = require("http");
 const express = require("express");
 const path = require("path");
-const ejs = require("ejs");
 const fetch = require("node-fetch");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -104,24 +103,10 @@ wsServer.on("connection", (socketClient) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
 app.use(express.static('public'));
 
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
-
 app.get("/", (req, res) => {
-  res.render("index", {
-    mobile: mobile,
-    landline: `(${landline.slice(2, 5)}) ${landline.slice(
-      5,
-      8
-    )}-${landline.slice(8, 12)}`,
-  });
+  res.render("index");
 });
 
 // OUTGOING MESSAGE
