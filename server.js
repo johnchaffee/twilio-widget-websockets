4fceaa57252146519f630909515fd0100fc99f73
John Chaffee <johnchaffee@mac.com>
2021-12-02 18:55:04 -0800

    Move resetConversationCount() and getMessages() into app.get("/") route

--- server.js
+++ server.js
@@ -60,6 +60,41 @@ app.get("/", (req, res) => {
     .catch(function (err) {
       res.status(500).send({ error: "we done homie" });
     });
+
+  // Mark messages as read when clicking on conversation
+  async function resetConversationCount(conversation_id) {
+    console.log("resetConversationCount()");
+    try {
+      const result = await pool.query(
+        "UPDATE conversations SET unread_count = $1 WHERE conversation_id = $2",
+        [0, conversation_id]
+      );
+    } catch (err) {
+      console.error(err);
+      // res.send("Error " + err);
+    }
+    // Send conversation to websocket clients
+    updateWebsocketClient(conversationObject);
+  }
+
+  // Fetch all messages for selected conversation
+  async function getMessages(mobileNumberQuery) {
+    console.log("getMessages():");
+    try {
+      const result = await pool.query(
+        "SELECT * FROM messages WHERE mobile_number = $1 order by date_created desc limit $2",
+        [mobileNumberQuery, limit]
+      );
+
+      messages = result.rows.reverse();
+      messages.forEach((message) => {
+        message.type = "messageCreated";
+      });
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  }
 });
 
 // SEND OUTGOING MESSAGE
@@ -94,7 +129,7 @@ app.post("/messagesend", (req, res, next) => {
     },
     body: bodyParams,
   };
-  
+
   sendMessage(apiUrl, requestOptions)
     .then((result) => {
       console.log("SEND MESSAGE THEN -> SUCCESS");
@@ -110,7 +145,7 @@ app.post("/messagesend", (req, res, next) => {
       console.log("SEND MESSAGE FINALLY");
     });
 
-    async function sendMessage(apiUrl, requestOptions) {
+  async function sendMessage(apiUrl, requestOptions) {
     console.log("sendMessage()");
     const response = await fetch(apiUrl, requestOptions);
     const result = await response.json();
@@ -300,26 +335,6 @@ async function getConversations() {
   }
 }
 
-// GET ALL MESSAGES FROM DB
-// Fetch all messages for selected conversation from postgres db
-async function getMessages(mobileNumberQuery) {
-  console.log("getMessages():");
-  try {
-    const result = await pool.query(
-      "SELECT * FROM messages WHERE mobile_number = $1 order by date_created desc limit $2",
-      [mobileNumberQuery, limit]
-    );
-
-    messages = result.rows.reverse();
-    messages.forEach((message) => {
-      message.type = "messageCreated";
-    });
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-}
-
 // CREATE MESSAGE
 async function createMessage(request, response) {
   console.log("createMessage()");
@@ -379,22 +394,6 @@ async function updateConversation(request, response) {
       console.error(err);
       // res.send("Error " + err);
     }
-  }
-  // Send conversation to websocket clients
-  updateWebsocketClient(conversationObject);
-}
-
-// RESET CONVERSATION COUNT
-async function resetConversationCount(conversation_id) {
-  console.log("resetConversationCount()");
-  try {
-    const result = await pool.query(
-      "UPDATE conversations SET unread_count = $1 WHERE conversation_id = $2",
-      [0, conversation_id]
-    );
-  } catch (err) {
-    console.error(err);
-    // res.send("Error " + err);
   }
   // Send conversation to websocket clients
   updateWebsocketClient(conversationObject);
