fdf5b91a086d270ff42d418ad17a61d2fe581b73
John Chaffee <johnchaffee@mac.com>
2021-12-09 17:15:00 -0800

    Tweak sample messages

--- db-setup.sh
+++ db-setup.sh
@@ -16,7 +16,7 @@ CREATE TABLE messages (
 
 -- Create sample messages
 INSERT INTO messages (date_created, direction, twilio_number, mobile_number, conversation_id, body, media_url)
-  VALUES ('2021-11-18T22:18:14.000Z', 'outbound', '+18555080989', '+12065551212', '+18555080989;+12065551212', 'Outgoing message', 'https://demo.twilio.com/owl.png'), ('2021-11-18T22:14:00.000Z', 'inbound', '+18555080989', '+12065551212', '+18555080989;+12065551212', 'Reply from mobile', '');
+  VALUES ('2021-11-18T22:18:14.000Z', 'outbound', '+18555080989', '+12065551212', '+18555080989;+12065551212', 'Hello from Twilio', 'https://demo.twilio.com/owl.png'), ('2021-11-18T22:15:00.000Z', 'inbound', '+18555080989', '+12065551212', '+18555080989;+12065551212', 'Hi from Mobile', null);
 
 -- Create conversations table
 CREATE TABLE conversations (
