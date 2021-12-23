f93738a3a1f3a44698d1394cc6e7719fed7b431e
John Chaffee <johnchaffee@mac.com>
2021-12-23 15:02:57 -0800

    Update deploy script → Configure phone number webhooks

--- deploy.sh
+++ deploy.sh
@@ -36,32 +36,46 @@ INSERT INTO conversations (date_updated, conversation_id, contact_name, unread_c
 
 EOF
 
-# Create Twilio Event Streams webhooks endpoint
+# Create Twilio Incoming Webhook for phone number
 
-SINKCONFIGURATION=$(cat << EOF
-{
-    "batch_events": false,
-    "destination": "https://$APP_HOST_NAME.herokuapp.com/twilio-event-streams",
-    "method": "post"
-}
-EOF
-)
-
-SID=`curl -X POST https://events.twilio.com/v1/Sinks \
---data-urlencode "Description=$APP_HOST_NAME Webhooks" \
---data-urlencode "SinkConfiguration=$SINKCONFIGURATION" \
---data-urlencode "SinkType=webhook" \
+PHONE_METADATA=`curl "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/IncomingPhoneNumbers.json?Beta=false&PhoneNumber=$TWILIO_NUMBER" \
 -u $TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN`
 
-# Extract the SinkSid
+PHONE_SID=`echo $PHONE_METADATA | grep -o "sid\": \"PN\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w" | cut -c8-`
 
-SINK_SID=`echo $SID | grep -o "sid\": \"\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w" | cut -c8-`
-
-# Create the subscriptions
-
-curl -X POST https://events.twilio.com/v1/Subscriptions \
---data-urlencode "Description=\"Subscribe to 'sent' and 'received' messaging events\"" \
---data-urlencode "Types={\"type\": \"com.twilio.messaging.message.sent\", \"schema_version\": 1}" \
---data-urlencode "Types={\"type\": \"com.twilio.messaging.inbound-message.received\", \"schema_version\": 1}" \
---data-urlencode "SinkSid=$SINK_SID" \
+curl -X POST "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/IncomingPhoneNumbers/$PHONE_SID.json" \
+--data-urlencode "SmsUrl=https://$APP_HOST_NAME.herokuapp.com/twilio-webhook" \
 -u $TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN
+
+
+# # Create Twilio Event Streams webhooks endpoint
+
+# SINKCONFIGURATION=$(cat << EOF
+# {
+#     "batch_events": false,
+#     "destination": "https://$APP_HOST_NAME.herokuapp.com/twilio-event-streams",
+#     "method": "post"
+# }
+# EOF
+# )
+
+# SID=`curl -X POST https://events.twilio.com/v1/Sinks \
+# --data-urlencode "Description=$APP_HOST_NAME Webhooks" \
+# --data-urlencode "SinkConfiguration=$SINKCONFIGURATION" \
+# --data-urlencode "SinkType=webhook" \
+# -u $TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN`
+
+# # Extract the SinkSid
+
+# SINK_SID=`echo $SID | grep -o "sid\": \"\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w\w" | cut -c8-`
+
+# # Create the subscriptions
+
+# curl -X POST https://events.twilio.com/v1/Subscriptions \
+# --data-urlencode "Description=\"Subscribe to 'sent' and 'received' messaging events\"" \
+# --data-urlencode "Types={\"type\": \"com.twilio.messaging.message.sent\", \"schema_version\": 1}" \
+# --data-urlencode "Types={\"type\": \"com.twilio.messaging.inbound-message.received\", \"schema_version\": 1}" \
+# --data-urlencode "SinkSid=$SINK_SID" \
+# -u $TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN
+
+
